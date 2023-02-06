1723f0dc10f1d67e4af666f41f26578bcf8e06ba
Tono <92106615+jmeno1011@users.noreply.github.com>
2023-02-06 18:05:21 +0900

    add comment and need to study

--- 프레임워크 없는 프론트엔드 개발/2장/04/registry.js
+++ 프레임워크 없는 프론트엔드 개발/2장/04/registry.js
@@ -1,7 +1,9 @@
 const registry = {}
 
+// 컴포넌트를 만들어주는 함수 component에 targetElement(타겟노드), state(데이터)를 넣어준다.
 const renderWrapper = component => {
   return (targetElement, state) => {
+    // 해설 필요
     const element = component(targetElement, state)
 
     const childComponents = element
@@ -26,6 +28,9 @@ const renderWrapper = component => {
   }
 }
 
+// 예시 파라메터: ('todos', todosView)
+// todos는 html코드에 data-component의 값이 todos라고 되어 있는 컴포넌트를 찾는 부분이다.
+// todosView는 todos에 해당하는 컴포넌트를 만들어주는 함수이다.
 const add = (name, component) => {
   registry[name] = renderWrapper(component)
 }
