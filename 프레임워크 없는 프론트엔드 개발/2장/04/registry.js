413870387ae723737e178c6a4153cda96a3a3a6b
Tono <92106615+jmeno1011@users.noreply.github.com>
2023-02-07 12:17:50 +0900

    add renderWrapper comment

--- 프레임워크 없는 프론트엔드 개발/2장/04/registry.js
+++ 프레임워크 없는 프론트엔드 개발/2장/04/registry.js
@@ -6,16 +6,18 @@ const renderWrapper = component => {
     // 해설 필요
     const element = component(targetElement, state)
 
+    // data-component 속성을 가진 모든 DOM 요소를 찾는다
     const childComponents = element
       .querySelectorAll('[data-component]')
 
+    // 자식 구성 요소를 호출
     Array
       .from(childComponents)
       .forEach(target => {
         const name = target
           .dataset
           .component
-
+        // data-component속성의 값을 키값으로 구성요소를 호출
         const child = registry[name]
         if (!child) {
           return
@@ -31,10 +33,13 @@ const renderWrapper = component => {
 // 예시 파라메터: ('todos', todosView)
 // todos는 html코드에 data-component의 값이 todos라고 되어 있는 컴포넌트를 찾는 부분이다.
 // todosView는 todos에 해당하는 컴포넌트를 만들어주는 함수이다.
+// 따라서 registry의 키는 data-component 속성 값과 일치한다.
 const add = (name, component) => {
   registry[name] = renderWrapper(component)
 }
 
+// root : root-element
+// state : data
 const renderRoot = (root, state) => {
   const cloneComponent = root => {
     return root.cloneNode(true)
